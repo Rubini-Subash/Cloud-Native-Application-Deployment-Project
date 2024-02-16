2bb6c33be7c8c6093a36e349ae308d296c88e44d
N Dinesh Kumar <ndinesh.kumar@energyexemplar.com>
2024-02-16 16:12:06 +0000

    Updated app.py

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Summer")
-option_b = os.getenv('OPTION_B', "Rainy")
+option_a = os.getenv('OPTION_A', "Vanilla")
+option_b = os.getenv('OPTION_B', "Mango")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
